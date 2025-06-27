bfbc97c0c1a8b6b7cdee88d777d06748af9bc03f
Theekshana Thennakoon <130481339+theekshana-thennakoon@users.noreply.github.com>
2025-06-27 16:37:04 +0530

    Add files via upload

---
+++ admin/include/header.php
@@ -0,0 +1,15 @@
+ <header class="app-header"><a class="app-header__logo" href="./" style = "background:#00008b;">Vehicle Record</a>
+      <a style = "background:#00008b;" class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+
+      <ul class="app-nav" style = "background:#00008b;">
+ 
+        <!-- User Menu-->
+        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i> <strong>Welcome Back : </strong>Admin </a>
+          <ul class="dropdown-menu settings-menu dropdown-menu-right">
+            <li><a class="dropdown-item" href="change-password.php"><i class="fa fa-cog fa-lg"></i> Change Password</a></li>
+            <li><a class="dropdown-item" href="profile.php"><i class="fa fa-user fa-lg"></i> Profile</a></li>
+            <li><a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </header>
